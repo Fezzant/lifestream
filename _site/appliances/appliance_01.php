7d9b166e69c493da84edb0c82d2f2d9b0209a51e
Fezzant <spitfire999@hotmail.co.uk>
2015-04-07 18:56:20 +0100

    Responsive buttons added
    JS to give the user responsive click down/up buttons on the web app

--- _site/appliances/appliance_01.php
+++ _site/appliances/appliance_01.php
@@ -3,34 +3,39 @@
     <head>
         <meta charset="utf-8" />
     </head>
- 
     <body>
 		<div style="width: 600px; height: 500px; background: #ccc;">
+			<?php
+			//Initialise the buttons in their "up" positions
+			echo ("<img id='button_power' src='assets/img/powerUp.png' alt='off'/>");
+			echo ("<img id='button_progSelect' src='assets/img/progSelectUp.png' alt='off'/>");
+			echo ("<img id='button_progStart' src='assets/img/progStartUp.png' alt='off'/>");
 
-			<?php
-			 //this php script generate the first page in function of the gpio's status
-			 $status = array (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-			 for ($i = 0; $i < count($status); $i++) {
-				//set the pin's mode to output and read them
-				system("gpio mode ".$i." out");
-				exec ("gpio read ".$i, $status[$i], $return );
-				//if off
-				if ($status[$i][0] == 0 ) {
-				echo ("<img id='button_".$i."' src='assets/img/powerOff.png' alt='off'/>");
+			//Getting and using pin and status values.
+			if (isset ($_GET["pin"]) && isset($_GET["status"]) ) {
+				$pin = strip_tags($_GET["pin"]);
+				$status = strip_tags($_GET["status"]);
+				//Ensuring the values are numbers and within the pin limit of the pi.
+				if ( (is_numeric($pin)) && (is_numeric($status)) && ($pin <= 40) && ($pin >= 0) && ($status == "0") || ($status == "1") ) {
+					//Toggle the selected gpio pin on, wait 50ms, then off.
+					system("gpio mode ".$pin." out");
+					$status = "1";
+					system("gpio write ".$pin." ".$status );
+					exec ("gpio read ".$pin, $status, $return );
+					echo ( $status[1] );
+					usleep(50000);
+					$status = "0";
+					system("gpio write ".$pin." ".$status );
+					exec ("gpio read ".$pin, $status, $return );
+					echo ( $status[0] );
 				}
-				//if on
-				if ($status[$i][0] == 1 ) {
-				echo ("<img id='button_".$i."' src='assets/img/powerOn.png' alt='on'/>");
-				}	 
-			 }
-			?>
-			
+			}
+			?>			
 			<?php
-				echo file_get_contents("washingMachineStatus.html");
-			?>
-			
+				//Retrieve the current washing machine status to be printed on screen.
+					echo file_get_contents("../washingMachineStatus.html");
+			?>		
 		</div>
-	
-		<script src="script.js"></script>
+		<script src="assets/js/gpio.js"></script>
     </body>
 </html>
---
+++ assets/js/gpio.js
@@ -0,0 +1,66 @@
+﻿//The JavaScript using images as buttons to send and receive values to/from the Pi
+var button_progSelect = document.getElementById("button_progSelect");
+var button_progStart = document.getElementById("button_progStart");
+var button_power = document.getElementById("button_power");
+
+//This function sends and receives the pins' status.
+function change_pin (pin, status) {
+	//HTTP request.
+	var request = new XMLHttpRequest();
+	request.open( "GET" , "/appliances/appliance_01.php?pin=" + pin + "&status=" + status );
+	request.send(null);
+	//Receiving values.
+	request.onreadystatechange = function () {
+		if (request.readyState == 4 && request.status == 200) {
+			return (parseInt(request.responseText));
+		}
+		else if (request.readyState == 4 && request.status == 500) {
+			alert ("server error");
+			return ("fail");
+		}
+		else { return ("fail"); }
+	}
+}
+
+//These are the button events, they call the change_pin function and updates the page based on its' return value.
+button_progSelect.addEventListener("mousedown", function () {
+	var new_status = change_pin ( 8, 0);
+	if (new_status !== "fail") { 
+		button_progSelect.alt = "on";
+		button_progSelect.src = "assets/img/progSelectDown.png";
+		return 0;
+	}
+} );
+button_progSelect.addEventListener("mouseup", function () {
+	button_progSelect.alt = "off";
+	button_progSelect.src = "assets/img/progSelectUp.png";
+	return 0;
+} );
+
+button_progStart.addEventListener("mousedown", function () {
+	var new_status = change_pin ( 9, 0);
+	if (new_status !== "fail") { 
+		button_progStart.alt = "on";
+		button_progStart.src = "assets/img/progStartDown.png";
+		return 0;
+	}
+} );
+button_progStart.addEventListener("mouseup", function () {
+	button_progStart.alt = "off";
+	button_progStart.src = "assets/img/progStartUp.png";
+	return 0;
+} );
+
+button_power.addEventListener("mousedown", function () {
+	var new_status = change_pin ( 12, 0);
+	if (new_status !== "fail") { 
+		button_power.alt = "on";
+		button_power.src = "assets/img/powerDown.png";
+		return 0;
+	}
+} );
+button_power.addEventListener("mouseup", function () {
+	button_power.alt = "off";
+	button_power.src = "assets/img/powerUp.png";
+	return 0;
+} );
